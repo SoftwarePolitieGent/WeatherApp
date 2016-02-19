d8f21133c01c5be5c917c8a325de53deab3777e2
SoftwarePolitieGent <software@politie.gent.be>
2016-02-19 10:54:02 +0100

    Initiële commit voor testproject Cross Platform

---
+++ Shared/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+// Source : https://msdn.microsoft.com/en-US/library/dn879698.aspx
+namespace Shared
+{
+    public class DataService
+    {
+        public static async Task<dynamic> getDataFormService(string queryString)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(queryString);
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+            var stream = response.GetResponseStream();
+
+            var streamReader = new StreamReader(stream);
+            string responseText = streamReader.ReadToEnd();
+
+            dynamic data = JsonConvert.DeserializeObject(responseText);
+
+            return data;
+
+        }
+
+    }
+}
---
+++ WeatherAppWindowsphone/Main.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Directive;
+using Shared;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WeatherAppWindowsphone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GetWeatherButton_Click(object sender, RoutedEventArgs e) {
+            Weather weather = Core.GetWeather(txtZipCode.Text).Result;
+           
+            if (weather != null)
+            {
+                ResultsTitle.Text = weather.Title;
+                TempText.Text = weather.Temperature;
+                WindText.Text = weather.Wind;
+                VisibilityText.Text = weather.Visibility;
+                HumidityText.Text = weather.Humidity;
+                SunriseText.Text = weather.Sunrise;
+                SunsetText.Text = weather.Sunset;
+
+                btnGetWeather.Content = "Search Again";
+
+            }
+            else
+            {
+                ResultsTitle.Text = "Couldn't find any results";
+            }
+
+        }
+    }
+}
