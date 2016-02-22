8e83369dbc5c1ccece6fd1a0367df5d725b92f55
SoftwarePolitieGent <software@politie.gent.be>
2016-02-22 16:18:07 +0100

    Domein invoeren

---
+++ SharedProject/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject
+{
+    public class DataService
+    {
+        public static async Task<dynamic> getDataFormService(string queryString)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(queryString);
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+            var stream = response.GetResponseStream();
+
+            var streamReader = new StreamReader(stream);
+            string responseText = streamReader.ReadToEnd();
+
+            dynamic data = JsonConvert.DeserializeObject(responseText);
+
+            return data;
+
+        }
+    }
+}
